65a02ba942bd1fbbc6e594b7b85fc14323b8720a
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-24 11:51:47 -0300

    views created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './views/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
