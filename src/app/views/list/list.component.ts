67b9a373e3d7e5c15b4e45c807fb3c1feeaaa545
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-24 14:14:35 -0300

    list done

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pokemon } from 'src/app/types/pokemon';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  filter: string = '';
+  selectedPokemon: Pokemon | null = null;
+
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemonService.listAll();
+  }
+
+  get pokemons(): Pokemon[] {
+    const filteredPokemons: Pokemon[] = this.filterPokemons(this.pokemonService.pokemons);
+    return filteredPokemons;
+  }
+
+  get pokemonSrc(): string | void {
+    if (this.selectedPokemon) {
+      const number = ('000' + this.selectedPokemon.number).slice(-3);
+      return `https://serebii.net/sunmoon/pokemon/${number}.png`
+    }
+  }
+
+  selectPokemon(pokemon: any): void {
+    this.selectedPokemon = pokemon;
+  }
+
+  private filterPokemons(pokemons: Pokemon[]): Pokemon[] {
+    const filteredPokemons: Pokemon[] = pokemons.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(this.filter.toLowerCase())
+    });
+
+    return filteredPokemons;
   }
 
 }
